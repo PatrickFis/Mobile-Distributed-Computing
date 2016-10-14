a8d89be336a1202e088859acc7ba3aaaa65362d9
Patrick Fischer <fischerpl@mail.lipscomb.edu>
2016-10-13 20:59:19 -0500

    Added chapter 6 directory.

---
+++ Chap6/Exercise6-9.c
@@ -0,0 +1,20 @@
+/*
+ * Exercise 6-9 - Write your own version of MPI_Reduce using functions
+ * MPI_Send and MPI_Recv. You may assume datatype = MPI_INT, operator = MPI_SUM,
+ * and comm = MPI_COMM_WORLD.
+ *
+ */
+#include <mpi.h>
+#include <math.h>
+#include <stdio.h>
+#include "MyMPI.h"
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+
+  /* Start the timer */
+  MPI_Barrier(MPI_COMM_WORLD);
+  elapsed_time = -MPI_Wtime();
+  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+}
