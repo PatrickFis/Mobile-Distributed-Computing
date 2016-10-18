0a66cdc0a2a723a6dc26a7674533dd17ff15ab95
Patrick Fischer <fischerpl@mail.lipscomb.edu>
2016-10-17 21:47:22 -0500

    So passing sendBuf and recvBuf as non-pointer integers to the new myReduce function will allow MPI_Recv to work. Need to come up with a solution to manipulate a reference of recvBuf. May need to change it to an int pointer and manipulate it inside the function.

--- Chap6/Exercise6-9.c
+++ Chap6/Exercise6-9.c
@@ -20,11 +20,23 @@
 //     MPI_Send(&sendBuf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 //   }
 // }
+void myReduce(int sendBuf, int recvBuf, int numProcesses, int id, MPI_Status status) {
+  if(id == 0) {
+    MPI_Recv(&sendBuf, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    int *temp = &recvBuf;
+    *temp += sendBuf;
+    printf("temp = %d\n", *temp);
+  }
+  else {
+    MPI_Send(&sendBuf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  }
+}
+
 int main(int argc, char *argv[]) {
   int id;
   int p;
   int sum = 0; // Local sum
-  int global_sum = 0;
+  int global_sum = 0; // Global sum
   MPI_Status status;
 
   MPI_Init(&argc, &argv);
@@ -33,15 +45,22 @@ int main(int argc, char *argv[]) {
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
+  // if(id != 0) {
+  //   sum = 1337;
+  //   MPI_Send(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  // }
+  // else {
+  //   for(int i = 1; i < p; i++) {
+  //     MPI_Recv(&sum, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+  //     global_sum += sum;
+  //   }
+  // }
   if(id != 0) {
     sum = 1337;
-    MPI_Send(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    myReduce(sum, global_sum, p, id, status);
   }
   else {
-    for(int i = 1; i < p; i++) {
-      MPI_Recv(&sum, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      global_sum += sum;
-    }
+    myReduce(sum, global_sum, p, id, status);
   }
   // printf("sum = %d, id = %d\n", sum,id);
   // myReduce(&sum, &global_sum, id, status);
